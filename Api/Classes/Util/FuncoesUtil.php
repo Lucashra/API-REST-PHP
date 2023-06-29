e8a8c8890198b261d27a62e093a5c2c6a54b10ba
Lucas Alessandro <lucas_hra@hotmail.com>
2023-06-28 22:27:02 -0300

    Classe de funções Util "função de debug"

---
+++ Api/Classes/Util/FuncoesUtil.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Util;
+
+abstract class FuncoesUtil
+{
+    public static function print_rpre($dado = null)
+    {
+        echo "<pre>";
+        print_r($dado);
+        echo"</pre>";exit;
+    }
+}
